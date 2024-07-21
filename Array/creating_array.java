aeaac15f225d21ac63d643444992e6e8231733c7
Henshi2622 <141411007+Henshi2622@users.noreply.github.com>
2024-07-21 12:24:42 +0530

    Declaring the array
    Array: Linear data structure that store elements of same type placed in contiguous memory location.
    
    All the elements are "0",when array is empty or has no element.

---
+++ Array/creating_array.java
@@ -0,0 +1,16 @@
+public class creating_array {
+    public static void main(String agrs[]){
+
+        String sub[]= new String[5];//declaring array
+        int marks[]=new int[5];// declaring array 
+
+        // printing array elements
+        for(int i =0;i<marks.length;i++){
+            System.out.println(marks[i]);
+        }
+
+        for(int i =0;i<sub.length;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
