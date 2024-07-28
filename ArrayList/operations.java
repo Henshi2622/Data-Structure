21ba17df9ecccf12c74c4f57e0f26e9b59f2ea4f
Henshi patel <141411007+Henshi2622@users.noreply.github.com>
2024-07-28 17:41:38 +0530

    Operations on Array list
    Unlike Array Arraylist is dynamic in nature tht means size is not fixed.
     we cannot store primitive datatypes directly so we use wrapper classes.
     Arraylist is a part of java collection frame work that only stores objects not primitive datatypes.

--- ArrayList/operations.java
+++ ArrayList/operations.java
@@ -1,1 +1,39 @@
 
+import java.util.ArrayList;
+public class operations {
+
+  public static void main(String[] args) {
+    ArrayList<Integer> list =new ArrayList<>();  
+    // add operation  O(n)
+    list.add(1);
+    list.add(2);
+    list.add(4);
+    list.add(8);
+    System.out.println(list);
+    
+    list.add(1,78);   //O(n)
+    System.out.println(list);
+
+    // get operation   O(n)
+    System.out.println(list.get(3));
+
+    //remove element   O(n^2)
+    list.remove(1);
+    System.out.println(list);
+
+    //set element at index    O(n)
+    list.set(2,34);
+    System.out.println(list);
+
+    //contains element  O(n)
+    //check whether element is present in an array or not
+    //return true if element is present otherwise return false
+
+    System.out.println(list.contains(8));
+
+    // size method
+    System.out.println(list.size());
+     
+   }
+}
+   
