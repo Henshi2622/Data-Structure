d9562e7648dab199949ca4ba839e46a806fca86b
Henshi patel <141411007+Henshi2622@users.noreply.github.com>
2024-08-17 21:09:14 +0530

    Inorder

---
+++ Binary tree/inorder.java
@@ -0,0 +1,48 @@
+public class inorder {
+    public static class node{
+        int data;
+        node left;
+        node right;
+        node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+
+    }
+    // to build tree
+    public static class Binarytree{
+        static  int idx=-1;
+        public static node buildtree(int arr[]){
+            idx++;
+            if(arr[idx]==-1){
+                return null;
+            }
+            node newnode=new node(arr[idx]);
+            newnode.left = buildtree(arr);
+            newnode.right = buildtree(arr);
+            return newnode;
+        }
+
+        // inorder
+    public static void  findinorder(node root){
+
+        if(root==null){
+            return ;
+        }
+        findinorder(root.left);
+        System.out.print(root.data +" ");
+        findinorder(root.right);
+
+       }
+    }
+
+    
+    public static void main(String[] args) {
+        int arr[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Binarytree  tree=new Binarytree();
+        node root=tree.buildtree(arr);
+        tree.findinorder(root);
+    }
+    
+}
